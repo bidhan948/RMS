140dfc197ae42fb8cf6899fa7c2a65db646e5bf6
BIDHAN BANIYA <bidhanbaniya789@gmail.com>
2022-10-03 19:04:24 +0545

    Dashboard set up

---
+++ routes/web.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+
+Auth::routes();
+
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+});
