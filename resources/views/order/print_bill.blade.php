17a70b8195cc02ddc1fef8b99cfac78d142b525b
BIDHAN BANIYA <bidhanbaniya789@gmail.com>
2023-01-05 19:34:27 +0545

    Print bill made

--- resources/views/order/print_bill.blade.php
+++ resources/views/order/print_bill.blade.php
@@ -5,7 +5,6 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
@@ -17,90 +16,92 @@
             margin: 0;
         }
 
-        #receipt_heading {
-            padding-left: 120px;
-        }
+        /*#receipt_heading {*/
+        /*    padding-left: 20px;*/
+        /*}*/
 
-        .ml-105 {
-            margin-left: 105px !important;
-        }
+        /*.ml-105 {*/
+        /*    margin-left: 55px !important;*/
+        /*}*/
 
-        .ml-110 {
-            margin-left: 110px !important;
-        }
+        /*.ml-110 {*/
+        /*    margin-left: 20px !important;*/
+        /*}*/
     </style>
 </head>
 <div class="container-fluid my-2">
     <div class="row">
-        <div class="col-4">
-            <p class="font-weight-bold mb-0">
-                <strong>
-                    @for ($i = 0; $i < 32; $i++)
-                        *
-                    @endfor
-                </strong>
-            </p>
-            <h4 id="receipt_heading">RECEIPT</h4>
-            <p class="font-weight-bold mb-0">
-                <strong>
-                    @for ($i = 0; $i < 32; $i++)
-                        *
-                    @endfor
-                </strong>
-            </p>
-            <span id="receipt_heading" class="mb-0">
-                {{ date('Y-m-d') }}
+        {{-- <!--<div class="col-4">-->
+            <!--<p class="font-weight-bold mb-0">-->
+            <!--    <strong>-->
+            <!--        @for ($i = 0; $i < 5; $i++)-->
+            <!--            *-->
+            <!--        @endfor-->
+            <!--    </strong>-->
+            <!--</p>--> --}}
+            <p style="text-align:center; font-size:0.6rem;font-weight:bold; ">The Coffe Bar</p>
+            <span style="text-align:center; font-size:0.5rem;" style="">Kanchanbari, Biratnagar</span><br>
+            <span style="text-align:center; font-size:0.5rem;">Estimate Bill</span>
+            {{-- <!--<p class="font-weight-bold mb-0">-->
+            <!--    <strong>-->
+            <!--        @for ($i = 0; $i < 5; $i++)-->
+            <!--            *-->
+            <!--        @endfor-->
+            <!--    </strong>-->
+            <!--</p>--> --}}
+            <span id="receipt_heading" class="mb-0" style="font-size:0.3rem; text-align:center;">
+                {{ date('Y-m-d H:i:s') }}
             </span>
-            <p class="font-weight-bold mb-0 mt-0">
-                <strong>
-                    @for ($i = 0; $i < 34; $i++)
-                        -
-                    @endfor
-                </strong><br>
-                <strong>
-                    @for ($i = 0; $i < 34; $i++)
-                        -
-                    @endfor
-                </strong>
-            </p>
-            <table class="table table-borderless">
-                <thead>
+            {{-- <!--<p class="font-weight-bold mb-0 mt-0">-->
+            <!--    <strong>-->
+            <!--        @for ($i = 0; $i < 5; $i++)-->
+            <!--            --->
+            <!--        @endfor-->
+            <!--    </strong><br>-->
+            <!--    <strong>-->
+            <!--        @for ($i = 0; $i < 5; $i++)-->
+            <!--            --->
+            <!--        @endfor-->
+            <!--    </strong>-->
+            <!--</p>--> --}}
+            <table class="table table-borderless" style="margin-top:-2px !important;">
+                <thead style="font-size:0.5rem;">
                     <tr>
                         <th>Item</th>
                         <th>Quantity</th>
                         <th>Price</th>
                     </tr>
                 </thead>
-                <tbody>
+                <tbody style="font-size:0.4rem !important;">
                     @foreach ($orders as $order)
-                        <tr>
-                            <td style="font-size: 0.8rem;">{{$order->Item->name}}</td>
-                            <td style="font-size: 0.8rem;">{{$order->quantity}}</td>
-                            <td style="font-size: 0.8rem;">{{$order->total}}</td>
+                        <tr style="font-size:0.4rem;">
+                            <td >{{$order->Item->name}}</td>
+                            <td >{{$order->quantity}}</td>
+                            <td >{{$order->total}}</td>
                         </tr>
                     @endforeach
                     <tr>
-                        <td class="text-center" colspan="2" style="font-size: 0.8rem;">Discount :</td>
-                        <td style="font-size: 0.8rem;">{{$orders[0]->discount .' %'}}</td>
+                        <td class="text-center" colspan="2" >Discount :</td>
+                        <td >{{$orders[0]->discount .' %'}}</td>
                     </tr>
                     <tr>
-                        <td class="text-center" colspan="2" style="font-size: 0.8rem;">Total :</td>
-                        <td style="font-size: 0.8rem;">{{$orders[0]->paid_amount}}</td>
+                        <td class="text-center" colspan="2" >Total :</td>
+                        <td >{{$orders[0]->paid_amount}}</td>
                     </tr>
                 </tbody>
             </table>
-            <p class="font-weight-bold mb-0">
-                <strong>
-                    @for ($i = 0; $i < 12; $i++)
-                        *
-                    @endfor
-                    THANK YOU
-                    @for ($i = 0; $i < 12; $i++)
-                        *
-                    @endfor
-                </strong>
-            </p>
-        </div>
+            {{-- <!--<p class="font-weight-bold mb-0">-->
+            <!--    <strong>-->
+            <!--        @for ($i = 0; $i < 3; $i++)-->
+            <!--            *-->
+            <!--        @endfor-->
+            <!--        THANK YOU-->
+            <!--        @for ($i = 0; $i < 3; $i++)-->
+            <!--            *-->
+            <!--        @endfor-->
+            <!--    </strong>-->
+            <!--</p>-->
+        <!--</div>--> --}}
     </div>
 </div>
 
